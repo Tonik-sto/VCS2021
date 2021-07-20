d10e5f786b2cfe30a1c711708437d300ab2aca21
Antonina Stoliarova <antonina.sto@gmail.com>
2021-07-20 17:18:00 +0300

    Homework Commit to the GitHub

---
+++ DropDown_Homework/Pages/DropDownPage.cs
@@ -0,0 +1,89 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropDown_Homework.Pages 
+{
+    public class DropDownPage : BasePage
+    {
+        private const string PageAddress = "https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html";
+        private const string ResultText = "Options selected are : ";
+        private SelectElement _MultiDropDown => new SelectElement(Driver.FindElement(By.Id("multi-select")));
+        private IWebElement _FirstSelectedButton => Driver.FindElement(By.Id("printMe"));
+        private IWebElement _GetAllSelectedButton => Driver.FindElement(By.Id("printAll"));
+        private IWebElement _Selected => Driver.FindElement(By.CssSelector(".getall-selected"));
+
+        public DropDownPage(IWebDriver webdriver) : base(webdriver)
+        {
+            Driver.Url = PageAddress;
+        }
+
+        public DropDownPage SelectFromMultipleDropdownByValue(List<string> listOfStates)
+        {
+            _MultiDropDown.DeselectAll();
+            Actions action = new Actions(Driver);
+            action.KeyDown(Keys.LeftControl);
+            foreach (string state in listOfStates)
+            {
+                foreach (IWebElement option in _MultiDropDown.Options)
+                {
+                    if (state.Equals(option.GetAttribute("value")))
+                    {
+                        action.Click(option);
+                        break;
+                    }
+                }
+            }
+            action.KeyUp(Keys.LeftControl);
+            action.Build().Perform();
+            _MultiDropDown.DeselectAll();
+            action.Click(_FirstSelectedButton);
+            action.Build().Perform();
+            return this;
+        }
+        public DropDownPage ClickGetAllButton()
+        {
+            _GetAllSelectedButton.Click();
+            return this;
+        }
+        /*
+        public void SelectFromMultipleDropDownByValue1(string firstValue, string secondValue)
+        {
+            Actions actions = new Actions(Driver);
+            _MultiDropDown.SelectByValue(firstValue);
+            actions.KeyDown(Keys.Control);
+            _MultiDropDown.SelectByValue(secondValue);
+            actions.KeyUp(Keys.Control);
+            actions.Build().Perform();
+        }
+        public DropDownPage ClickFirstSelectedButton()
+        {
+            _FirstSelectedButton.Click();
+            return this;
+        }
+
+        public DropDownPage ClickAllSelectedButton()
+        {
+            _GetAllSelectedButton.Click();
+            return this;
+        */
+
+        public DropDownPage VerifyFirstSelected(string selectedCountry)
+        {
+            Assert.IsTrue(_Selected.Text.Equals(ResultText + selectedCountry), $"Result is wrong, not {selectedCountry}");
+            return this;
+        }
+
+        public DropDownPage VerifyAllSelected(params string[] selectedElements)
+        {
+            Assert.IsTrue(_Selected.Text.Equals(ResultText + selectedElements), $"Result is wrong, not {selectedElements}");
+            return this;
+        }
+    }
+}
---
+++ DropDown_Homework/Tests/DropDownTest.cs
@@ -0,0 +1,56 @@
+﻿using DropDown_Homework.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropDown_Homework.Tests
+{
+    public class DropDownTest
+    {
+        private static DropDownPage _page;
+
+        [OneTimeSetUp]
+        public static void SetUp()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            driver.Manage().Window.Maximize();
+            _page = new DropDownPage(driver);
+        }
+
+        [OneTimeTearDown]
+
+        public static void TearDown()
+        {
+            //_page.CloseBrowser();
+        }
+        
+        [TestCase("New Jersey", "California", TestName = "Pasirenkame 2 reiksmes ir patikriname First Selected")]
+        [TestCase("New Jersey", "California", "Ohio", TestName = "Pasirenkame 3 reiksmes ir patikriname First Selected")]
+        public void TestMultipleDropdown1Selected(params string[] selectedElements)
+        {
+            _page.SelectFromMultipleDropdownByValue(selectedElements.ToList())
+                .VerifyFirstSelected(selectedElements[2]);
+        }
+        
+        [TestCase("New Jersey", "California", TestName = "Pasirenkame 2 reiksmes ir patikriname Get All Selected")]
+        [TestCase("New Jersey", "California", "Ohio", "Washington", TestName = "Pasirenkame 4 reiksmes ir patikriname Get All Selected")]
+        public void TestMultipleDropdownAllSelected(params string[] selectedElements)
+        {
+            _page.SelectFromMultipleDropdownByValue(selectedElements.ToList())
+                            .ClickGetAllButton()
+                            .VerifyAllSelected(selectedElements);
+        }
+
+
+        //1) Pažymime 2 valstijas, spaudžiame "First Selected" mygtuką - patikrinam, ar rezultatas teisingas, rodo pirmą pažymėtą valstiją.
+        //2) Pažymime 2 valstijas, spaudžiame "Get All Selected" mygtuką - patikrinam, ar rezultatas teisingas, rodo visas užžymėtas valstijas.
+        //3) Pažymime 3 valstijas, spaudžiame "First Selected" mygtuką - patikrinam, ar rezultatas teisingas, rodo pirmą pažymėtą valstiją.
+        //4) Pažymime 4 valstijas, spaudžiame "Get All Selected" mygtuką - patikrinam, ar rezultatas teisingas, rodo visas užžymėtas valstijas.
+    }
+}
